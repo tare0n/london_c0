f17882a775497d868c586953c233de2fb877f028
misha kalinin <msktareo@gmail.com>
2022-12-07 23:19:09 +0300

    v1.2 of main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from london_co import london_co
 def ask():
     input_name = input("Введите имя устройства: ")
     if input_name in london_co.keys():
-        print(london_co[input_name])
+        input_param = input("Введите имя параметра: ")
+        if input_param in london_co[input_name].keys():
+            print(london_co[input_name][input_param])
 
 ask()
