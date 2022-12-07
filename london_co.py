2f8c9e56bf1f30a8a6672f7bbab61bd7297f552e
misha kalinin <msktareo@gmail.com>
2022-12-07 22:47:31 +0300

    add london_co dict

---
+++ london_co.py
@@ -0,0 +1,25 @@
+london_co = {
+        "r1": {
+            "location": "21 New Globe Walk",
+            "vendor": "Cisco",
+            "model": "4451",
+            "ios": "15.4",
+            "ip": "10.225.0.1"
+        },
+        "r2": {
+            "location": "21 New Globe Walk",
+            "vendor": "Cisco",
+            "model": "4451",
+            "ios": "15.4",
+            "ip": "10.225.0.2"
+        },
+        "sw1": {
+            "location": "21 New Globe Walk",
+            "vendor": "Cisco",
+            "model": "3850",
+            "ios": "3.6.XE",
+            "ip": "10.225.0.101",
+            "vlans": "10,20,30",
+            "routing": True
+        }
+    }
